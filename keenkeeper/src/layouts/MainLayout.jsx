0011aa8d5ac7b187a047cb6e947742762151be73
Md Towfiqur Rahman <140777602+towfiqurv360@users.noreply.github.com>
2026-04-16 18:06:55 +0600

    feat: setup main layout structure

---
+++ keenkeeper/src/layouts/MainLayout.jsx
@@ -0,0 +1,19 @@
+import { Outlet } from "react-router-dom";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+
+const MainLayout = () => {
+  return (
+    <div className="min-h-screen bg-slate-950 flex flex-col">
+      <Navbar />
+
+      <main className="flex-1">
+        <Outlet />
+      </main>
+
+      <Footer />
+    </div>
+  );
+};
+
+export default MainLayout;
