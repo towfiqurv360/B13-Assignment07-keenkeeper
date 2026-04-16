b5023f58c06bb4779edf90af2abf8582f7e2ef86
Md Towfiqur Rahman <140777602+towfiqurv360@users.noreply.github.com>
2026-04-16 17:49:21 +0600

    Initial structure

---
+++ keenkeeper/src/App.jsx
@@ -0,0 +1,42 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import MainLayout from "./layouts/MainLayout";
+import Home from "./pages/Home";
+import FriendDetails from "./pages/FriendDetails";
+import Timeline from "./pages/Timeline";
+import Stats from "./pages/Stats";
+import NotFound from "./pages/NotFound";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainLayout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/friend/:id",
+        element: <FriendDetails />,
+      },
+      {
+        path: "/timeline",
+        element: <Timeline />,
+      },
+      {
+        path: "/stats",
+        element: <Stats />,
+      },
+    ],
+  },
+  {
+    path: "*",
+    element: <NotFound />,
+  },
+]);
+
+function App() {
+  return <RouterProvider router={router} />;
+}
+
+export default App;
